1169ce84adaa5d50d289d5bcec672ad056b67b13
jharri34 <harrisjayjamall@gmail.com>
2025-03-20 09:49:18 -0400

    added bells and whistls for demo

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import os
 
+
 from PIL import Image
+
 from os import listdir
 from os.path import isfile, join
 from transformers import BlipProcessor, BlipForConditionalGeneration
@@ -35,6 +37,26 @@ def caption_image(image_file):
     except Exception as e:
         print(f"Error processing {image_file}: {e}")
 
+def compress_image(image):
+    with  Image.open(image) as uncompressed_image:
+        
+        # the original width and height of the image
+        image_height = uncompressed_image.height
+        image_width = uncompressed_image.width
+
+        print("The original size of Image is: ", round(len(uncompressed_image.fp.read())/1024,2), "KB")
+
+        #compressed the image
+        compressed_image = uncompressed_image.resize((image_width,image_height),Image.NEAREST)
+
+        #save the image
+        print(f"compressed image name: {image.name}" )
+        compressed_image.save(f"{DATA_PATH}/compressed-{image.name}")
+
+        #open the compressed image
+        with Image.open(os.path.join(DATA_PATH,f"compressed-{image.name}")) as compressed_image:
+            print("The size of compressed image is: ", round(len(compressed_image.fp.read())/1024,2), "KB")
+    return compressed_image 
 
 def main():
     uploaded_file = None
@@ -44,8 +66,16 @@ def main():
         uploaded_file = st.file_uploader("Choose a Image file",  type=['jpg', 'jpeg', 'png'])
         if uploaded_file is not None:
             st.write("filename:", uploaded_file.name)
+            
     if uploaded_file is not None:
-        st.image(uploaded_file, caption=caption_image(uploaded_file))        
+        caption = f"""<img src="{uploaded_file.name}" alt="{caption_image(uploaded_file)}">"""
+        st.image(uploaded_file, caption=caption)
+        st.code(caption, language=None)
+        with Image.open(uploaded_file) as uncompressed_image:
+            st.write("The original size of Image is: ", round(len(uncompressed_image.fp.read())/1024,2), "KB")
+            compressed_image = compress_image(uploaded_file)
+            with Image.open(compressed_image.filename) as compresed_image:
+                st.write("The size of compressed image is: ", round(len(compresed_image.fp.read())/1024,2), "KB")
     #         with open(os.path.join(DATA_PATH,uploaded_file.name), "wb") as f:                
     #             f.write(uploaded_file.getvalue())
     #             st.success("Saved Files")
